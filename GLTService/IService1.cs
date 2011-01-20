efa17cf70318c4ce5a54ba194f0599a306a2ae0c
zhangqd <zhangqd01@163.com>
2011-01-20 22:04:17 +0800

    First Version service include x509Certificate

---
+++ GLTService/IService1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GLTService
+{
+    // NOTE: If you change the interface name "IService1" here, you must also update the reference to "IService1" in Web.config.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        int add(int a,int b);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
