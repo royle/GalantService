21fa6ec1fd2aec5e69e427b4383124890c88ed77
zhangqd <zhangqd01@163.com>
2011-05-22 16:29:00 +0800

    权限判断

--- Galant.DataEntity/AppStatusCach.cs
+++ Galant.DataEntity/AppStatusCach.cs
@@ -39,7 +39,142 @@ namespace Galant.DataEntity
         public Entity StaffCurrent
         {
             get { return staffCurrent; }
-            set { staffCurrent = value; OnPropertyChanged("StaffCurrent"); }
+            set
+            {
+                staffCurrent = value;
+                this.Powers = this.Getpowers(value);
+                OnPropertyChanged("StaffCurrent");
+                OnPropertyChanged("Powers");
+            }
+        }
+
+        /// <summary>
+        /// 是否有公司经理权限
+        /// </summary>
+        public bool IsShowHq
+        {
+            get
+            {
+                return this.IsManager || (from p in this.AllowStations
+                        where p.EntityType== EntityType.Headquarter select p).Count() > 0;
+            }
+        }
+
+        /// <summary>
+        /// 是否有公司经理权限
+        /// </summary>
+        public bool IsManager
+        {
+            get 
+            {
+                return (from p in Powers where p == RoleType.CustomerSupportManager || 
+                            p== RoleType.FinanceManager ||
+                            p== RoleType.GeneralManager ||
+                            p== RoleType.OperationManager select p).Count()>0;
+            }
+        }
+        /// <summary>
+        /// 是否显示客服功能
+        /// </summary>
+        public bool IsShowCustomerService
+        {
+            get
+            {
+                return (from p in Powers
+                        where p == RoleType.CustomerSupportManager ||
+                        p == RoleType.CustomerSupport ||
+                            p == RoleType.GeneralManager ||
+                            p == RoleType.OperationManager ||
+                            p == RoleType.StationManager
+                        select p).Count() > 0;
+            }
+        }
+
+        /// <summary>
+        /// 是否显示调度
+        /// </summary>
+        public bool IsShowRoute
+        {
+            get
+            {
+                return (from p in Powers
+                        where p == RoleType.CustomerSupportManager ||
+                         p == RoleType.CustomerSupport ||
+                            p == RoleType.GeneralManager ||
+                            p == RoleType.OperationManager ||
+                            p == RoleType.StationManager
+                        select p).Count() > 0;
+            }
+        }
+
+        /// <summary>
+        /// 是否显示水站
+        /// </summary>
+        public bool IsShowStation
+        {
+            get
+            {
+                return (from p in Powers
+                        where p == RoleType.CustomerSupportManager ||
+                         p == RoleType.CustomerSupport ||
+                            p == RoleType.GeneralManager ||
+                            p == RoleType.OperationManager ||
+                            p == RoleType.StationManager
+                        select p).Count() > 0;
+            }
+        }
+
+        /// <summary>
+        /// 是否显示系统管理
+        /// </summary>
+        public bool IsShowSystemAdmin
+        {
+            get
+            {
+                return (from p in Powers
+                        where p == RoleType.GeneralManager ||
+                            p == RoleType.OperationManager 
+                        select p).Count() > 0;
+            }
+        }
+
+        private List<RoleType> Getpowers(Entity user)
+        {
+            return user.Roles==null? new List<RoleType>() : (from r in user.Roles select r.RoleType).ToList();
+        }
+
+        public List<Entity> allowStations;
+
+        /// <summary>
+        /// 有权限的站点集合
+        /// </summary>
+        public List<Entity> AllowStations
+        {
+            get {
+                if (allowStations == null)
+                    allowStations = (this.StaffCurrent.Roles == null ? new List<Entity>() : (from r in StaffCurrent.Roles select r.Station).ToList());
+                return IsManager ? this.Stations : allowStations; }
+            set { allowStations = value; }
+        }
+
+        List<RoleType> powers;
+        /// <summary>
+        /// 权限
+        /// </summary>
+       [IgnoreDataMember]
+        public List<RoleType> Powers
+        {
+            get
+            {
+                if (powers == null)
+                    this.Getpowers(this.StaffCurrent);
+                return powers;
+            }
+            set
+            {
+                this.powers = value;
+                OnPropertyChanged("Powers");
+            }
         }
 
         private List<Entity> entities;
@@ -66,8 +201,9 @@ namespace Galant.DataEntity
         /// </summary>
         public List<Entity> Headquarters
         {
-            get { return Entities == null ? null : Entities.Where(e=>e.EntityType == EntityType.Headquarter).ToList(); }
+            get { return (this.IsManager || this.IsShowHq) ? (Entities == null ? null : Entities.Where(e => e.EntityType == EntityType.Headquarter).ToList()) : null; }
         }
+
 
         /// <summary>
         /// 站点集合
